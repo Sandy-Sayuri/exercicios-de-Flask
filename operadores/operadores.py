44e61ce2a515e82ef3fffa00ec6fd1749918a70f
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-08 17:22:19 -0300

    operadores

---
+++ operadores/operadores.py
@@ -0,0 +1,24 @@
+# Operadores Aritméticos
+print(3+2)
+print(2-3)
+print(10/3)
+print(10*3)
+print(10%3)
+print(3**2)
+print(3**3)
+#Operdores de Comparação
+b=5-3
+print(b==2)
+print(b!=3)
+print(b>3)
+print(b<3)
+print(b>=2)
+print(b>=3)
+#Operdores Lógicos
+a= True
+b=False
+print(a and b)
+print(a or b)
+print(not b)
+print(a and not b)
+print(not a and b)
