2a7cfe7d14528febf9777791d3305995f00497fd
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-08 18:41:39 -0300

    métodos

---
+++ metodos/metodos.py
@@ -0,0 +1,14 @@
+import re
+
+
+print('olá')
+def meu_metodo():
+    print('olá')
+    print('mundo')
+print('Do lado dentro')
+meu_metodo()
+print('oi')
+def soma_dos_valores(valor1,valor2):
+    soma=valor1+valor2
+    return soma
+print(soma_dos_valores(5,3))
