b3e20c4fc8b439d1060f3b25f9ae5a7e101594c1
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-10 09:02:06 -0300

    fazendo aulas

---
+++ loop (while e for)/loop (while e for).py
@@ -0,0 +1,7 @@
+minha_variavel='ola'
+print(len(minha_variavel))
+print(minha_variavel[0])
+print(minha_variavel[1])
+print(minha_variavel[2])
+#for ->para
+#while ->emquanto
--- metodos/metodos.py
+++ metodos/metodos.py
@@ -1,5 +1,3 @@
-import re
-
 
 print('olá')
 def meu_metodo():
@@ -7,8 +5,19 @@ def meu_metodo():
     print('mundo')
 print('Do lado dentro')
 meu_metodo()
-print('oi')
 def soma_dos_valores(valor1,valor2):
     soma=valor1+valor2
     return soma
 print(soma_dos_valores(5,3))
+print('daomdoas')
+
+print(len('ola'))
+print(len('ola '))
+x=-10
+print(abs(x))
+lista=[1,2,3,4]
+print(sum(lista))
+print(min(lista))
+print(max(lista))
+print(round(10,90))
+print(round(10.942332,2))
