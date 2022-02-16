3109078d0db91cdb56aea14c5817b917b04a47c5
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-16 10:18:48 -0300

    criando um rest

---
+++ Rest Api/resources/hotel.py
@@ -0,0 +1,16 @@
+hoteis=[{
+    'hotel_id':'alpha',
+    'nome':'Alpha Hotel',
+    'estrelas':4.3,
+    'diaria':420.34,
+    'cidades':'Rio de Janeiro'},
+    {'hotel_id':'bravo',
+    'nome':'Bravo Hotel',
+    'estrelas':4.4,
+    'diaria':380.90,
+    'cidades':'Santa Catarina'},
+    {'hotel_id':'charlie',
+    'nome':'    Charlie Hotel',
+    'estrelas':3.9,
+    'diaria':320.20,
+    'cidades':'Santa Catarina'}]
