91e3a2af0497dccadb54372e6711435b36eaf018
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-16 12:57:42 -0300

    mandar
    estão com problemas

--- Rest Api/resources/hotel.py
+++ Rest Api/resources/hotel.py
@@ -1,3 +1,6 @@
+from tkinter import NO
+from flask_restful import Resource,reqparse
+
 hoteis=[{
     'hotel_id':'alpha',
     'nome':'Alpha Hotel',
@@ -14,3 +17,43 @@ hoteis=[{
     'estrelas':3.9,
     'diaria':320.20,
     'cidades':'Santa Catarina'}]
+class Hoteis(Resource):
+    def get(self):
+        return{'hoteis':hoteis}
+class Hotel(Resource):
+    def buscar_hotel(hotel_id):
+        for hotel in hoteis:
+            if(hotel['hotel_id']==hotel_id):
+                return hotel
+        return None
+    def novo_hotel():
+        argumento=reqparse.RequestParser()
+        argumento.add_argument('nome')
+        argumento.add_argument('estrelas')
+        argumento.add_argument('diaria')
+        argumento.add_argument('cidades')
+    def get(self,hotel_id):
+        hotel=Hotel.buscar_hotel(hotel_id)
+        if hotel != None:
+            return hotel
+        return{'message':'Hotel not found'},404#not found
+    def post(self,hotel_id):
+        dados=novo_hotel.argumentos.parse_args()
+        novo_hotel={
+            'hotel_id':hotel_id,
+            'nome':dados['nome'],
+            'estrelas':dados['estrelas'],
+            'diaria':dados['diaria'],
+            'cidade':dados['cidade']
+        }
+        hoteis.append(novo_hotel)
+        return novo_hotel,200
+    def delete(self,hotel_id):
+        pass
+    def put(self,hotel_id):
+        dados=Hotel.argumentos.parse_args()
+        hotel=Hotel.buscar_hotel(hotel_id)
+        if hotel != None:
+            return hotel
+        return{'message':'Hotel not found'},404#not found
+        pass
