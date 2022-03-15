320231bad2c362e08d0e854ac654030ac4900cfa
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-03-15 11:22:52 -0300

    SUBIR

--- Rest Api/resources/hotel.py
+++ Rest Api/resources/hotel.py
@@ -1,31 +1,42 @@
+import re
 from tkinter import NO
 from flask_restful import Resource,reqparse
 
 hoteis=[{
-    'hotel_id':'alpha',
-    'nome':'Alpha Hotel',
-    'estrelas':4.3,
-    'diaria':420.34,
-    'cidades':'Rio de Janeiro'},
-    {'hotel_id':'bravo',
-    'nome':'Bravo Hotel',
-    'estrelas':4.4,
-    'diaria':380.90,
-    'cidades':'Santa Catarina'},
-    {'hotel_id':'charlie',
-    'nome':'    Charlie Hotel',
-    'estrelas':3.9,
-    'diaria':320.20,
-    'cidades':'Santa Catarina'}]
+    "hotel_id":"alpha",
+    "nome":"Alpha Hotel",
+    "estrelas":4.3,
+    "diaria":420.34,
+    "cidades":"Rio de Janeiro"
+    },
+    {
+    "hotel_id":"bravo",
+    "nome":"Bravo Hotel",
+    "estrelas":4.4,
+    "diaria":380.90,
+    "cidades":"Santa Catarina"
+    },
+    {
+    "hotel_id":"charlie",
+    "nome":"Charlie Hotel",
+    "estrelas":3.9,
+    "diaria":320.20,
+    "cidades":"Santa Catarina"
+    }]
 class Hoteis(Resource):
     def get(self):
         return{'hoteis':hoteis}
+    
+    
+    
 class Hotel(Resource):
     def buscar_hotel(hotel_id):
         for hotel in hoteis:
             if(hotel['hotel_id']==hotel_id):
                 return hotel
-        return None
+        return None #not found
+    
+    
     def novo_hotel():
         argumento=reqparse.RequestParser()
         argumento.add_argument('nome')
@@ -38,7 +49,13 @@ class Hotel(Resource):
             return hotel
         return{'message':'Hotel not found'},404#not found
     def post(self,hotel_id):
-        dados=novo_hotel.argumentos.parse_args()
+        argumetos=reqparse.RequestParser()
+        argumetos.add_argument('nome')
+        argumetos.add_argument('estrelas')
+        argumetos.add_argument('diaria')
+        argumetos.add_argument('cidade')
+        
+        dados=argumetos.parse_args()
         novo_hotel={
             'hotel_id':hotel_id,
             'nome':dados['nome'],
@@ -48,12 +65,11 @@ class Hotel(Resource):
         }
         hoteis.append(novo_hotel)
         return novo_hotel,200
+    def put(self,hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id']==hotel_id:
+                return hotel
     def delete(self,hotel_id):
         pass
-    def put(self,hotel_id):
-        dados=Hotel.argumentos.parse_args()
-        hotel=Hotel.buscar_hotel(hotel_id)
-        if hotel != None:
-            return hotel
-        return{'message':'Hotel not found'},404#not found
-        pass
+    
+        
