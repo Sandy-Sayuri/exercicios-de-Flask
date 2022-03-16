68a98c25d191904083c10ce1ed911379faca9233
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-03-16 08:03:32 -0300

    Update app.py

--- Rest Api/app.py
+++ Rest Api/app.py
@@ -3,9 +3,30 @@ from flask import Flask
 from flask_restful import Resource,Api
 app=Flask(__name__)
 api=Api(app)
+
+hoteis=[
+    {
+        'hotel_id':'alpha',
+        'nome':'Alpha Hotel',
+        'estrela':4.3,
+        'diaria':420.34
+    }
+    {
+        'hotel_id':'beta',
+        'nome':'Alpha Hotel',
+        'estrela':4.3,
+        'diaria':420.34
+    }
+    {
+        'hotel_id':'alpha',
+        'nome':'Alpha Hotel',
+        'estrela':4.3,
+        'diaria':420.34
+    }
+]
 class Hoteis(Resource):
     def get (self):
-        return{'hoteis':'meus hoteis'}
+        return {'hoteis':'meus hoteis'}
     
 api.add_resource(Hoteis,'/hoteis')
 
