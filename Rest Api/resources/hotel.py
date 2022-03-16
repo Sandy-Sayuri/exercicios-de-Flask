34438dc0f8fe6af57f16c5676a369a26ad13929d
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-03-16 10:20:44 -0300

    criando modes no hotel

--- Rest Api/resources/hotel.py
+++ Rest Api/resources/hotel.py
@@ -1,5 +1,6 @@
 import this
 from flask_restful import Resource,reqparse
+from models.hotel import HotelModel
 hoteis=[
     {
         'hotel_id':'alpha',
@@ -24,26 +25,6 @@ hoteis=[
         'cidade':'Santa Catarina'
     }
 ]
-
-class HotelModel:
-    def __init__(self,hotel_id,nome,estrelas,diaria,cidade):
-       self.hotel_id= hotel_id
-       self.nome=nome
-       self.estrelas=estrelas
-       self.diaria=diaria
-       self.cidade=cidade
-       
-    def json(self):
-        return{
-            'holtel_id':self.hotel_id,
-            'nome':self.nome,
-            'estrelas':self.estrelas,
-            'diaria':self.diaria,
-            'cidade':self.cidade
-        }   
-
-
-
 class Hoteis(Resource):
     def get (self):
         return {'hoteis':hoteis}
